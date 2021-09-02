dca9d83b0b26bf3a15d97c9f228269f19f2c55fe
ganuong <ganuonglachanh@outlook.com>
2021-09-02 17:28:31 +0700

    return post hint in search reust

--- src/Gambit/TitleGambit.php
+++ src/Gambit/TitleGambit.php
@@ -4,6 +4,8 @@ namespace GaNuongLaChanh\Sonic\Gambit;
 use Flarum\Search\SearchState;
 use GaNuongLaChanh\Sonic\Driver\MySqlDiscussionTitleDriver;
 use Flarum\Search\GambitInterface;
+use Flarum\Post\Post;
+use Illuminate\Database\Query\Expression;
 
 class TitleGambit implements GambitInterface
 {
@@ -32,9 +34,32 @@ class TitleGambit implements GambitInterface
 
         if (! isset($bit) || strlen($bit)<=3) return $search;
 
+        $query = $search->getQuery();
+        $grammar = $query->getGrammar();
+
         $relevantPostIds = $this->titleGambit->match($bit);
         $discussionIds = array_keys($relevantPostIds);
+        $postIdsArr = array_values($relevantPostIds);
+        $postIds = array();
+        array_walk_recursive($postIdsArr,function($v) use (&$postIds){ $postIds[] = $v; }); // flatten array
+        $subquery = Post::whereVisibleTo($search->getActor())
+            ->select(['id as most_relevant_post_id','discussion_id'])
+            ->whereIn('id', $postIds);
+
+        $query
+            ->addSelect('posts_ft.most_relevant_post_id')
+            ->join(
+                new Expression('(' . $subquery->toSql() . ') ' . $grammar->wrapTable('posts_ft')),
+                'posts_ft.discussion_id',
+                '=',
+                'discussions.id'
+            )
+            ->groupBy('discussions.id')
+            ->addBinding($subquery->getBindings(), 'join');
+
+
         $search->getQuery()->whereIn('id', $discussionIds);
-        $search->setDefaultSort(['id' => $discussionIds]);
+        //$search->setDefaultSort(['id' => $discussionIds]);
+        $search->setDefaultSort(['id' => 'desc']);
     }
 }
